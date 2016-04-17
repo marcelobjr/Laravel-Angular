084c1ad53ee13491002bd09763a28a856ef227f5
Marcelo <marcelobjr@hotmail.com>
2016-04-17 14:12:08 -0300

    Trabalhando com eventos

--- resources/assets/js/directives/projectFileDownload.js
+++ resources/assets/js/directives/projectFileDownload.js
@@ -1,35 +1,36 @@
 angular.module('app.directives')
-.directive('projectFileDownload', ['appConfig','ProjectFile', 
-	function(appConfig,ProjectFile){
+    .directive('projectFileDownload', ['$timeout', 'appConfig', 'ProjectFile',
+        function ($timeout, appConfig, ProjectFile) {
+            return {
+                restrict: 'E',
+                templateUrl: appConfig.baseUrl + '/build/views/templates/projectFileDownload.html',
+                link: function (scope, element, attr) {
+                    var anchor = element.children()[0];
+                    scope.$on('save-file', function (event, data) {
+                        $(anchor).removeClass('disabled');
+                        $(anchor).text('Download');
+                        $(anchor).attr({
+                            href: 'data:application-octet-stream;base64,' + data.file,
+                            download: data.name
+                        });
 
-	return {
-		restrict:'E',
-		templateUrl: appConfig.baseUrl + '/build/views/templates/projectFileDownload.html',
-		link: function(scope,element,attr){
-
-		},
-		controller: ['$scope','$attrs','$element','$timeout',
-		function($scope,$attrs,$element,$timeout){
-			$scope.downloadFile = function() {
-				var anchor = $element.children()[0];
-				$(anchor).addClass('disabled');
-				$(anchor).text('Loading...');
-				ProjectFile.download({id: null, idFile:$attrs.idFile}, 
-					function(data){
-						$(anchor).removeClass('disabled');
-						$(anchor).text('Save File');
-						$(anchor).attr({
-							href: 'data:application-octet-stream;base64,'+ data.file,
-							download: data.name
-						});
-						$timeout(function(){
-							$scope.downloadFile=function(){};
-							$(anchor)[0].click();
-						});
-						
-					});
-			};
-		}]
-
-	}
-}]);
+                        $timeout(function () {
+                            scope.downloadFile = function () {
+                            };
+                            $(anchor)[0].click();
+                        });
+                    });
+                },
+                controller: ['$scope', '$element', '$attrs', 
+                function ($scope, $elemnet, $attrs) {
+                    $scope.downloadFile = function () {
+                        var anchor = $elemnet.children()[0];
+                        $(anchor).addClass('disabled');
+                        $(anchor).text('Loading...');
+                        ProjectFile.download({id: $attrs.idProject, idFile: $attrs.idFile}, function (data) {
+                            $scope.$emit('save-file', data);
+                    });
+                }
+            }]
+        }
+    }]);
